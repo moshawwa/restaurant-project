f553533ce2b181237f5fb0d2afdbcfd1fa2dfa7c
moshawwa <mohammedshawa10@gmail.com>
2025-07-08 14:07:59 +0300

    add base ./ to vite config file

--- resturant/vite.config.ts
+++ resturant/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// vite.config.js
-export default {
+export default defineConfig({
+  plugins: [react()],
   base: "./",
-};
+});
