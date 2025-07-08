86095d4feab52dc05a3d6143d30d17d4a83ff583
moshawwa <mohammedshawa10@gmail.com>
2025-07-08 15:56:12 +0300

    edit name the same rebo name

--- resturant/vite.config.ts
+++ resturant/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/restaurant/', // Replace 'restaurant' with your actual GitHub repository name
+  base: '/restaurant-project/', // Replace 'restaurant' with your actual GitHub repository name
   build: {
     outDir: 'dist',
   },
